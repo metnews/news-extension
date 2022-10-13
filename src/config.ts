2948e1b7e73a486c1a9500cf8296ec8ffb97dc3c
Jian Li <kdlijian@gmail.com>
2022-10-13 08:18:11 +0800

    fix url configurations

--- src/config.ts
+++ src/config.ts
@@ -9,29 +9,23 @@ const env = process.env.NODE_ENV
 const development = {
 	articleStateURL: "http://localhost:8080/api/collection/state",
 	collectionURL: "http://localhost:8080/api/collection",
-	collectionsURL: "http://localhost:8080/collection/",
-
 	feedStateURL: "http://localhost:8080/api/feed/state",
-	feedURL: "http://localhost:8080/feed?id=",
 	subscribeURL: "http://localhost:8080/api/feed",
-
 	shareURL: "http://localhost:8080/api/share",
 
-	homeURL: "http://localhost:8080/"
+	collectionsURL: "http://localhost:8080/collection/",
+	feedURL: "http://localhost:8080/feed?id=",
 }
 
 const production = {
 	articleStateURL: "https://app.metnews.co/api/collection/state",
 	collectionURL: "https://app.metnews.co/api/collection",
-	collectionsURL: "https://app.metnews.co/collection/",
-
 	feedStateURL: "https://app.metnews.co/api/feed/state",
-	feedURL: "https://app.metnews.co/feed?id=",
 	subscribeURL: "https://app.metnews.co/api/feed",
+	shareURL: "https://app.metnews.co/api/share",
 
-	shareURL: "http://app.metnews.co/api/share",
-
-	homeURL: "https://metnews.co/"
+	collectionsURL: "https://app.metnews.co/collection/",
+	feedURL: "https://app.metnews.co/feed?id=",
 }
 
 const config: Config = {
